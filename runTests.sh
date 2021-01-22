74ee11e76b108623596865fecaa7d3fe09ffcc15
Joseph H <12785608+josephH00@users.noreply.github.com>
2021-01-22 15:41:31 -0700

    Fixed naming issue

--- runTests.sh
+++ runTests.sh
@@ -2,11 +2,24 @@
 
 # Joseph Hand CSE205 1/15/2021
 # javac Assignment*.java && java Assignment* < inputN.txt > myoutN.txt && diff outputN.txt myoutN.txt
-javac *.java
-for ((iter = 1 ; iter <= $1 ; iter++));
+if [ $# -lt 2 ]
+  then
+    echo "This script will compile and compare the output of the assignment (whose name is formatted as Assignment#.java where # is the assignment number) against the provided test cases (formated as input#.txt and output#.txt) used in CSE205"
+    echo "Usage:"
+    echo "./runTests.sh [ASSIGNMENT NUMBER] [NUMBER OF TEST CASES]"
+    echo
+    echo "Example:"
+    echo "   To test Assignment 2 against 4 test cases where Assignment2.java and the input#/output# files are in the same directory run './runTests.sh 2 4'"
+    exit
+fi
+
+javac Assignment$1.java
+for ((iter = 1 ; iter <= $2 ; iter++));
 do
     inFileName="input${iter}.txt"
     myOutFileName="myout${iter}.txt"
     compareFile="output${iter}.txt"
-    java Assignment* < $inFileName > $myOutFileName && diff $compareFile $myOutFileName
+
+    echo Test Case $iter:
+    java Assignment$1 < $inFileName > $myOutFileName && diff $compareFile $myOutFileName
 done
